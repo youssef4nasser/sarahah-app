e16cda20af7113a3417c4839b1089ee5ac9c5f35
youssef4nasser <youssef4nasser@gmail.com>
2024-02-23 16:51:46 +0200

    home page done

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,22 @@
+import Image from "next/image.js";
 import styles from "./page.module.css";
+import Link from "next/link.js";
 
 export default function Home() {
   return <>
-  <h1>home page</h1>
+  <section className="my-5">
+    <div className="container">
+      <div className="row">
+        <div className="col text-center ">
+          <Image src={"/logo.png"} className="" alt="logo sarahah" width={200} height={200} />
+          <h1>Sarahah</h1>
+        </div>
+        <div className="col-12 text-center my-4">
+          <Link href="/login" className="btn btn-info text-white my-3 w-100">Login</Link>
+          <Link href="/register" className="btn btn-info text-white my-3 w-100">Register</Link>
+        </div>
+      </div>
+    </div>
+  </section>
   </> 
 }
